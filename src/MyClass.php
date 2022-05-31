5189b0937038a8c3778d1abb10e3e02a58645aa0
Gianluca Magni <magni.c-@legrand-ext.com>
2022-05-31 13:30:12 +0200

    include math

--- src/MyClass.php
+++ src/MyClass.php
@@ -2,8 +2,14 @@
 
 namespace Packtest;
 
+use MathPHP\Algebra;
+
 class MyClass {
     public function dump($var) {
         var_dump($var);
+    }
+
+    public function doMath($num) {
+        $gcd = Algebra::gcd(8, $num);
     }
 }
